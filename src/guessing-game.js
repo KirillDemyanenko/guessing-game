3059f75beb182bce812891713f322210fc46daa5
KirillDemyanenko <113039556+KirillDemyanenko@users.noreply.github.com>
2023-02-24 13:39:06 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    max;
+    last;
+    min;
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.last = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.last = Math.ceil((this.max - this.min) / 2) + this.min;
+        return this.last;
     }
 
     lower() {
-
+        this.max = this.last
     }
 
     greater() {
-
+        this.min = this.last
     }
 }
-
 module.exports = GuessingGame;
